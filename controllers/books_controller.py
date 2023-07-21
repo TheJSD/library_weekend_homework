861dff883f40871f636e882467a3335d270b38c7
James Dickson <jamesstuartdickson@gmail.com>
2023-07-21 15:02:37 +0100

    added functionality to view individual books - created index.jinja and updated index.jinja

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -6,3 +6,8 @@ books_blueprint = Blueprint("books", __name__)
 @books_blueprint.route("/library")
 def index():
     return render_template("index.jinja", title="Library Catalog", books=books)
+
+@books_blueprint.route("/library/<index>")
+def book(index):
+    book = books[int(index) - 1]
+    return render_template("book.jinja", title="{book.title}", book=book)
