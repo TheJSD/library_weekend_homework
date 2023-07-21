29e292acd0d3a85819cf8b3063b56095d4749de7
James Dickson <jamesstuartdickson@gmail.com>
2023-07-21 12:55:56 +0100

    updated app.py, base.jinja and the books controller

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -3,6 +3,6 @@ from models.book_list import books
 
 books_blueprint = Blueprint("books", __name__)
 
-@books_blueprint.route("/books")
+@books_blueprint.route("/library")
 def index():
     return render_template("index.jinja", title="Library Catalog", books=books)
