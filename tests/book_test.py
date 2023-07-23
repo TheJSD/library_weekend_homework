66a5e6cb0bb12ef58220cbabf0613557ed6434cb
James Dickson <jamesstuartdickson@gmail.com>
2023-07-23 20:39:34 +0100

    updated unittesting

--- tests/book_test.py
+++ tests/book_test.py
@@ -1,22 +1,31 @@
 import unittest
 from models.book import Book
-from models.book_list import BookMethods, book1, book2, book3, books
+from models.book_list import BookMethods, book1, book2, book3
 
 class TestBook(unittest.TestCase):
 
     def setUp(self):
-        self.book1 = book1
-        self.book2 = book2
-        self.book3 = book3
-        self.books = books
+        self.book1 = Book("The Colour of Magic", "Terry Pratchett", "Fantasy")
+        self.book2 = Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", "Science-Fiction")
+        self.book3 = Book("The Art of War", "Sun Tzu", "Treatise")
+        self.books = [book1, book2, book3]
     
     def test_add_book(self):
         book4 = Book("Varieties of Daedra", "Aranea Drethan", "Fantasy")
-        BookMethods.add_book(book4)
+        BookMethods.add_book(self.books, book4)
         number_of_books = len(self.books)
         self.assertEqual(4, number_of_books)
     
     def test_remove_book(self):
-        
+        BookMethods.remove_book(self.books, 0)
         number_of_books = len(self.books)
-        self.assertEqual(4, number_of_books)
+        self.assertEqual(2, number_of_books)
+
+    def test_check_in_book(self):
+        self.books[0].checked_out = True
+        BookMethods.check_in_book(self.books, 0)
+        self.assertEqual(False, self.books[0].checked_out)
+    
+    def test_check_out_book(self):
+        BookMethods.check_out_book(self.books, 0)
+        self.assertEqual(True, self.books[0].checked_out)
