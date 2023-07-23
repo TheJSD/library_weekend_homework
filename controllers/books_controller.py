ed1578bb030d444c51c4ee827a7dc7cf89d44bbc
James Dickson <jamesstuartdickson@gmail.com>
2023-07-23 19:48:44 +0100

    added CSS and gave a function to change whether a book is checked in or out through a checkbox

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,5 +1,5 @@
 from flask import render_template, Blueprint, request
-from models.book_list import books, add_book, remove_book
+from models.book_list import books, add_book, remove_book, check_in_book, check_out_book
 from models.book import Book
 
 books_blueprint = Blueprint("books", __name__)
@@ -12,7 +12,7 @@ def index():
 def book(index):
     book = books[int(index) - 1]
     book_index = index
-    return render_template("book.jinja", title="{book.title}", book=book, book_index=book_index)
+    return render_template("book.jinja", title=book.title, book=book, book_index=book_index)
 
 @books_blueprint.route("/library/add_a_book")
 def add_a_book_site():
@@ -28,3 +28,14 @@ def add_a_book():
 def remove_book_from_library(index):
     remove_book((int(index) - 1))
     return render_template("index.jinja", title="Library Catalog", books=books)
+
+@books_blueprint.route("/library/<index>/check_out_toggle", methods=["post"])
+def check_out_toggle(index):
+    book = books[int(index) - 1]
+    book_id = int(index)
+    is_checked_out = request.form.get("check_out")
+    if is_checked_out == "Check out":
+        check_out_book(book_id - 1)
+    else:
+        check_in_book(book_id - 1)
+    return render_template("book.jinja", title=book.title, book=book, book_index=book_id)
--- models/book_list.py
+++ models/book_list.py
@@ -1,6 +1,7 @@
 from models.book import Book
 
 book1 = Book("The Colour of Magic", "Terry Pratchett", "Fantasy")
+book1.checked_out = True
 book2 = Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", "Science-Fiction")
 book3 = Book("The Art of War", "Sun Tzu", "Treatise")
 
@@ -11,3 +12,9 @@ def add_book(book):
 
 def remove_book(book_index):
     del books[book_index]
+
+def check_in_book(book_index):
+    books[book_index].checked_out = False
+
+def check_out_book(book_index):
+    books[book_index].checked_out = True
